5244767d85e2182de509a62946b05685ab6ec059
Manoj Gautam <surfer.manoj@gmail.com>
2015-07-15 08:58:22 +0545

    API Code to send message, some comment discription is added

--- send_message.py
+++ send_message.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 # Download the twilio-python library from http://twilio.com/docs/libraries
-from twilio.rest import TwilioRestClient
+from twilio.rest import TwilioRestClient #Twilio rest API need install sudo pip install twilio
  
 # Find these values at https://twilio.com/user/account
-account_sid = "__ACbde3ae0895cf6e2836c27db1356e84be"
-auth_token = "__b2466fc7618a64d73c85ae12d9c9a4fe"
+account_sid = "__ACbde3ae0895cf6e2836c27db1356e84be" #Unique for each account equivalent to twilio username
+auth_token = "__b2466fc7618a64d73c85ae12d9c9a4fe"   #unique to twilio account password
 client = TwilioRestClient(account_sid, auth_token)
  
 message = client.messages.create(to="+977984317589", from_="+16066539735",
